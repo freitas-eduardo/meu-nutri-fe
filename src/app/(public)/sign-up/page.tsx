f36f637125cac1c827391150dd00c674b33ef4ab
Eduardo Freitas <freitas.eduardo.gon@gmail.com>
2023-10-14 21:07:08 -0300

    pass signup to client component

--- src/app/(public)/sign-up/page.tsx
+++ src/app/(public)/sign-up/page.tsx
@@ -1,13 +1,14 @@
+'use client'
 import React from "react";
 import PublicLayout from "../layout";
 import { useSearchParams } from "next/navigation";
 import { Typography } from "antd";
 
-// import { Container } from './styles';
+
 
 const SignUp: React.FC = () => {
   const searchParams = useSearchParams();
-  const signUpType = searchParams.get("type") || "patient";
+
 
   return (
     <PublicLayout>
